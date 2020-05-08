3277c250c2ac713ffd76c467126845aaa60987eb
gipuntoesse <giuliosqueo.mail@gmail.com>
2020-05-08 20:01:01 +0200

    Added field name, getter and setter

--- src/it/portal/model/User.java
+++ src/it/portal/model/User.java
@@ -1,5 +1,14 @@
 package it.portal.model;
 
 public class User {
+	public String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
